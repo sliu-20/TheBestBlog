a6357ebde4e481afd4e14a1422b17d3e3691c790
cnelson20 <cjnelson2004@gmail.com>
2021-11-05 09:21:59 -0400

    fixed bug

--- app/app.py
+++ app/app.py
@@ -140,7 +140,7 @@ def all_blogs():
 
     my = False;
     # my_blog function
-    if ('a' in request.args and 'a' == 't' and 'username' in session):
+    if ('a' in request.args and request.args['a'] == 't' and 'username' in session):
         c.execute("SELECT * FROM BLOGS WHERE AUTHOR = ?;",(session['username'],));
         results = c.fetchall();
         my = True
